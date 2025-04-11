3cc2eb641f8faecc9036954ddf82b52e36fb685b
viniciochavess <viniciochavess@gmail.com>
2025-04-10 21:39:45 -0300

    styles: criação da página de login

--- src/pages/auth/Sign-In/index.tsx
+++ src/pages/auth/Sign-In/index.tsx
@@ -1,10 +1,33 @@
+import { Button } from "../../../components/ui/button";
+import { Input } from "../../../components/ui/input";
+import { Label } from "../../../components/ui/label";
+
 export function SignIn() {
   return (
     <>
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Sign In</h1>
-
-    </div>
+     <div className="p-8">
+         <div className="flex w-[350px] flex-col justify-center gap-6">
+           <div className="flex flex-col gap-2 text-center">
+             <h1 className="text-2xl font-semibold tracking-tight">
+               Acessar painel
+             </h1>
+             <p className="text-sm text-muted-foreground">
+               Acompanhe suas vendas pelo painel do parceiro!
+             </p>
+           </div>
+ 
+           <form className="space-y-4">
+             <div className="space-y-2">
+               <Label htmlFor="email">Seu e-mail</Label>
+               <Input id="email" type="email" />
+             </div>
+ 
+             <Button className="w-full cursor-pointer" type="submit">
+               Acessar painel
+             </Button>
+           </form>
+         </div>
+       </div>
     </>
   );
 }
